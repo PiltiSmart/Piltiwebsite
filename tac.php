586180986ce485d34aecb0d9c26c999f95fb41a0
viswanathanTJ <viswanathantj@gmail.com>
2021-06-03 16:52:40 +0530

    SD-132 Terms and Conditions

--- tac.php
+++ tac.php
@@ -1,71 +1,300 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<title>Pilti - Everything can be Smart</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="description" content="" />
-<meta name="author" content="http://webthemez.com" />
- 
-<link href="css/bootstrap.min.css" rel="stylesheet" />
-<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
-<link href="css/jcarousel.css" rel="stylesheet" />
-<link href="css/flexslider.css" rel="stylesheet" />
-<link href="css/style.css" rel="stylesheet" />
- 
-<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-<!--[if lt IE 9]>
+    <meta charset="utf-8">
+    <title>Pilti - Everything can be Smart</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+    <meta name="author" content="http://webthemez.com" />
+
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+    <link href="css/jcarousel.css" rel="stylesheet" />
+    <link href="css/flexslider.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
 </head>
-<body>
-<div id="wrapper">
 
-	<!-- start header -->
-		<header>
-        <div class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo"/></a>
-                </div>
-                <div class="navbar-collapse collapse ">
-                    <ul class="nav navbar-nav">
-                    <li><a href="index.php">Home</a></li>  
-						<li><a href="about.php">About Us</a></li>
-            <li><a href="pilticloud.php">Pilti Cloud</a></li>
-						<li class="active"><a href="services.php">Services</a></li>
-                        <li><a href="pricing.php">Pricing</a></li>
-                        <li><a href="contact.php">Contact</a></li>
-                    </ul>
+<body>
+    <div id="wrapper">
+
+        <!-- start header -->
+        <header>
+            <div class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo" /></a>
+                    </div>
+                    <div class="navbar-collapse collapse ">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About Us</a></li>
+                            <li><a href="pilticloud.php">Pilti Cloud</a></li>
+                            <li class="active"><a href="services.php">Services</a></li>
+                            <li><a href="pricing.php">Pricing</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-	</header><!-- end header -->
-	<section id="content">
-		<div class="container content">	
-        </div>
+        </header><!-- end header -->
+        <section id="content">
+            <div class="container content">
+
+                <header class="entry-header">
+                    <h1 class="entry-title">Terms and Conditions</h1>
+                </header><!-- .entry-header -->
+
+                <div class="entry-content">
+
+                    <p>The vast majority of design projects run smoothly, without any problems. However, from time to
+                        time, something does come up, and these terms and conditions are designed to ensure that we are
+                        both protected from any unexpected circumstances. It also lays out the basic groundwork for how
+                        the project will progress, and what’s expected of us both. </p>
+
+
+
+                    <p><strong>Summary</strong></p>
+
+
+
+                    <p>The following Terms and Conditions apply to all products and services provided by Helen Murphy
+                        Freelance Graphic Design (hereinafter referred to as Helen Murphy or ‘I’)&nbsp;</p>
+
+
+
+                    <p>By accepting a quote, you are considered to have accepted these terms and conditions in full.</p>
+
+
+
+                    <p><strong>What do both parties agree to do?</strong></p>
+
+
+
+                    <p>You agree that: You have the authority to enter into this contract on behalf of yourself, your
+                        company or your organization. You agree to provide me with everything that I need to complete
+                        the project including text, images and other information as and when I need it, and in the
+                        format that I ask for. You’ll review my work and provide feedback and approval in a timely
+                        manner. You also agree to stick to the payment schedule set out in these terms and conditions.
+                    </p>
+
+
+
+                    <p>I agree that: I have the experience and ability to perform the services you need from me and I
+                        will carry them out in a professional and timely manner. Along the way I will endeavour to meet
+                        all the deadlines set but I can’t be responsible for a missed deadline if materials have been
+                        supplied late or have not approved or signed off my work on-time at any stage. I will maintain
+                        the confidentiality of any information that you give me.</p>
+
+
+
+                    <p><strong>Project Delivery Dates</strong></p>
+
+
+
+                    <p>If there is a hard deadline for your project, you must communicate it clearly up front before the
+                        project begins. It is always my intention to complete your project on time. Revisions and your
+                        response times play a major role in the amount of time it takes to complete your project.
+                        Therefore, the project completion date I give you is not a guarantee.</p>
+
+
+
+                    <p><strong>Design Services</strong></p>
+
+
+
+                    <p>These Terms and Conditions allow for up to three sets of revisions unless agreed otherwise prior
+                        to starting the project.&nbsp; Extra revisions are subject to additional charges at my
+                        discretion. &nbsp;</p>
+
+
+
+                    <p>You should supply graphic files in an editable, vector digital format. You should supply
+                        photographs in a high resolution digital format. If you choose to buy stock photographs, I can
+                        suggest stock libraries. If you’d like me to search for photographs for you, I can provide a
+                        separate estimate for that. If you need any advice on these formats I’m always happy to help.
+                    </p>
+
+
+
+                    <p>I’m not responsible for writing or editing any text copy unless specified in the original
+                        estimate. If you’d like me to write new content or edit text for you, I can provide a separate
+                        estimate for that.</p>
+
+
+
+                    <p>If you want to add anything new or change your mind about something as the project progresses
+                        that won’t be a problem. I’m happy to be flexible and will provide a seperate quote as required.
+                    </p>
+
+
+
+                    <p>If a choice of designs is presented and one is chosen for your project, only that solution is
+                        deemed to be given by us as fulfilling the contract. All other designs remain the property of
+                        Helen Murphy, unless specifically agreed in writing.</p>
+
+
+
+                    <p>Please note: Once you approve a design it is considered final. After this any changes requested
+                        will be quoted and billed seperately. Be sure to proof read carefully before approving the final
+                        design.</p>
+
+
+
+                    <p><strong>Errors and Omissions</strong></p>
+
+
+
+                    <p>It is the Client’s responsibility to check proofs carefully for accuracy in all respects
+                        including but not limited to prices, spelling, and dimensions. Helen Murphy is not liable for
+                        errors or omissions. The Client’s written approval of an artwork proof, will be taken as
+                        confirmation that the project is correct and complete.</p>
+
+
+
+                    <p><strong>Project Cancellations</strong></p>
+
+
+
+                    <p>Should something happen for any reason whatsoever that causes you to cancel this project once it
+                        has begun, you are completely free to do so. You will simply be billed for any work completed up
+                        to the point of cancellation, and I will send you any work completed up to that point.</p>
+
+
+
+                    <p>In the unlikely event that some life altering event happens to cause me to cancel this project, I
+                        will hand over any work completed up to the point of cancellation and refund you for any
+                        payments made on incomplete work.</p>
+
+
+
+                    <p><strong>Copyrights</strong></p>
+
+
+
+                    <p>You guarantee that all elements of text, graphics, photos, designs, trademarks, or other artwork
+                        you provide are either owned by your company, or that you have permission to use them.</p>
+
+
+
+                    <p>Copyright is retained by Helen Murphy on all design work including words, pictures, ideas,
+                        visuals and illustrations until all costs have been settled. When your final payment has
+                        cleared, copyright will be automatically assigned as follows:</p>
+
+
+
+                    <p>You’ll own the visual elements that I create for this project. I will give you the finished
+                        print-ready or digital files and you should keep them somewhere safe as I’m not required to keep
+                        a copy. You own all elements of text, images and data you provided, unless someone else owns
+                        them.</p>
+
+
+
+                    <p>I’ll own the source files unless we agree otherwise.&nbsp;</p>
+
+
+
+                    <p>I reserve the right, unless you specifically ask me not to, to display your project as part of my
+                        portfolio and for use on social media. </p>
+
+
+
+                    <p><strong>Payment</strong></p>
+
+
+
+                    <p>Payment is due within 30 days of date of invoice. Payments should be made by bank transfer,
+                        referencing the invoice number. Payments made by cheque must be previously agreed and may be
+                        subject to an administration charge.</p>
+
+
+
+                    <p>Publication and/or release of work done by Helen Murphy on behalf of the client, may not take
+                        place before cleared funds have been received.</p>
+
+
+
+                    <p>Helen Murphy reserves the right to charge statutory interest of 8% plus the Bank of England base
+                        rate under the late payment legislation on overdue balances.</p>
+
+
+
+                    <p>Helen Murphy is responsible for the payment of all government taxes with respect to the services
+                        she performs for the Client as a sole trader.</p>
+
+
+
+                    <p><strong>Disclaimer</strong></p>
+
+
+
+                    <p>Helen Murphy will not be responsible for any damages your business may suffer. I can not
+                        guarantee that my work will be error-free and so I can’t be liable to you or any third-party for
+                        damages, including lost profits, lost savings or other incidental, consequential or special
+                        damages, even if you’ve advised me of them. Finally, if any provision of this contract shall be
+                        unlawful, void, or for any reason unenforceable, then that provision shall be deemed severable
+                        from this contract and shall not affect the validity and enforceability of any remaining
+                        provisions.&nbsp;</p>
+
+
+
+                    <p>Helen Murphy reserves the right to revise its policies, Terms and Conditions at any time.</p>
+
+
+
+                    <p><strong>Acceptance of Terms</strong></p>
+
+
+
+                    <p>By accepting a quotation and supplying information/images/files to alllow a project to commence,
+                        the Client agrees that they have read, understood, and are considered legally bound to these
+                        terms.</p>
+
+                </div><!-- .entry-content -->
+
+                </article><!-- #post-## -->
+
+
+
+                </main><!-- #main -->
+            </div><!-- #primary -->
+
+    </div><!-- .content-wrapper -->
+
+
+    </div><!-- #content -->
+
+
+    </div>
     </section>
-	<?php include_once 'footer.php';?>
-</div>
-<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
-<!-- javascript
+    <?php include_once 'footer.php';?>
+    </div>
+    <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+    <!-- javascript
     ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="js/jquery.js"></script>
-<script src="js/jquery.easing.1.3.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.fancybox.pack.js"></script>
-<script src="js/jquery.fancybox-media.js"></script> 
-<script src="js/portfolio/jquery.quicksand.js"></script>
-<script src="js/portfolio/setting.js"></script>
-<script src="js/jquery.flexslider.js"></script>
-<script src="js/animate.js"></script>
-<script src="js/custom.js"></script>
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/jquery.fancybox-media.js"></script>
+    <script src="js/portfolio/jquery.quicksand.js"></script>
+    <script src="js/portfolio/setting.js"></script>
+    <script src="js/jquery.flexslider.js"></script>
+    <script src="js/animate.js"></script>
+    <script src="js/custom.js"></script>
 </body>
+
 </html>
