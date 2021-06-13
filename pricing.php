349fb451ddd9af75f38487dec1e96e0982cd24a8
viswanathan59 <viswanathantj@gmail.com>
2021-06-13 10:45:25 +0530

    Services Page hidden

--- pricing.php
+++ pricing.php
@@ -37,7 +37,7 @@
                     <ul class="nav navbar-nav">
                         <li><a href="index.php">Home</a></li>  
 						<li><a href="about.php">About Us</a></li>
-						<li><a href="services.php">Services</a></li>
+						<!-- <li><a href="services.php">Services</a></li> -->
                         <li class="active"><a href="pricing.html">Pricing</a></li>
                         <li><a href="contact.php">Contact</a></li>
                         <li><a href="pilticloud.php">Pilti Cloud</a></li>
