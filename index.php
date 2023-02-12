f1f7fe10459adbb8904f0ab0a4f12a2292f1105d
Umesh kandhalu <umesh@piltismart.com>
2023-02-12 14:19:16 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>PiltiSmart - Everything can be SMART </title>
+  <title>PiltiSmart - Everything can be SMART  </title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="description">
   <!-- css -->
